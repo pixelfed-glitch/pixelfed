8d7a7992055815d10de4d4547c733da6d3ab4267
Mehdi Benadel <mehdi.benadel@gmail.com>
2025-01-17 23:07:17 +0100

    Merge docker updates from jippi/docker-pixelfed
    Co-authored-by: Christian Winther <jippignu@gmail.com>

---
+++ docker/rootfs/shared/docker/entrypoint.d/11-first-time-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# shellcheck disable=SC2119
+
+: "${ENTRYPOINT_ROOT:="/docker"}"
+
+# shellcheck source=SCRIPTDIR/../helpers.sh
+source "${ENTRYPOINT_ROOT}/helpers.sh"
+
+entrypoint-set-script-name "$0"
+
+load-config-files
+
+# Allow automatic applying of outstanding/new migrations on startup
+: "${DOCKER_APP_RUN_ONE_TIME_SETUP_TASKS:=1}"
+
+if is-false "${DOCKER_APP_RUN_ONE_TIME_SETUP_TASKS}"; then
+    log-warning "Automatic run of the 'One-time setup tasks' is disabled."
+    log-warning "Please set [DOCKER_APP_RUN_ONE_TIME_SETUP_TASKS=1] in your [.env] file to enable this."
+
+    exit 0
+fi
+
+# Wait for the database to be ready
+await-database-ready
+
+# Make sure only one container run the remainder of this script at a time
+acquire-lock
+
+# Following https://docs.pixelfed.org/running-pixelfed/installation/#one-time-setup-tasks
+#
+# NOTE: Caches happens in [30-cache.sh]
+
+only-once "key:generate" run-as-runtime-user php artisan key:generate
+only-once "storage:link" run-as-runtime-user php artisan storage:link
+only-once "initial:migrate" run-as-runtime-user php artisan migrate --force
+only-once "import:cities" run-as-runtime-user php artisan import:cities
+
+if is-true "${ACTIVITY_PUB:-false}"; then
+    only-once "instance:actor" run-as-runtime-user php artisan instance:actor
+fi
+
+if is-true "${OAUTH_ENABLED:-false}"; then
+    only-once "passport:keys" run-as-runtime-user php artisan passport:keys
+fi
