2c7d7539d7d95dd64a34879db6625feb6a21d011
Mehdi Benadel <mehdi.benadel@gmail.com>
2025-01-20 00:11:55 +0100

    flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Dev Environment flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs }:
 
