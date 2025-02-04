f25c999766a0fb8768cdf1e7c28bffe304b17421
Uthanien <feldarec@gmail.com>
2025-02-04 17:10:32 -0300

    New variable for lan spanish on auth, profile, helpcenter, navmenu and settings

---
+++ resources/lang/es/helpcenter.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+	'helpcenter' => 'Centro de Ayuda',
+	'whatsnew' => 'What\'s New',
+
+	'gettingStarted' => 'Getting Started',
+	'sharingMedia' => 'Sharing Media',
+	'profile' => 'Perfil',
+	'stories' => 'Historias',
+	'hashtags' => 'Hashtags',
+	'discover' => 'Descubre',
+	'directMessages' => 'Mensajes Directos',
+	'timelines' => 'Timelines',
+	'embed'	=> 'Embed',
+
+	'communityGuidelines' => 'Community Guidelines',
+	'whatIsTheFediverse' => 'Que es el fediverso?',
+	'controllingVisibility' => 'Controlando la Visibilidad',
+	'blockingAccounts' => 'Bloqueando Cuentas',
+	'safetyTips' => 'Tips de Seguridad',
+	'reportSomething' => 'Reportar Algo',
+	'dataPolicy' => 'Politica sobre datos',
+
+	'taggingPeople' => 'Etiquetando Personas'
+
+];
---
+++ resources/lang/es/navmenu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	'search'			=> 'Buscar',
+	'home'				=> 'Inicio',
+	'local'				=> 'Local',
+	'network'			=> 'Network',
+	'discover'			=> 'Descubre',
+	'viewMyProfile'		=> 'Ver mi perfil',
+	'myProfile'			=> 'Mi Perfil',
+	'myTimeline'		=> 'Mi Timeline',
+	'publicTimeline'	=> 'Timeline publico',
+	'remoteFollow'		=> 'Segudior Remoto',
+	'settings'			=> 'Configuraciones',
+	'admin'				=> 'Admin',
+	'logout'			=> 'Logout',
+	'directMessages'	=> 'Mensajes Directos',
+	'composePost'		=> 'Crear Post',
+];
