ef607f4468797b414402dc7bb1cee0e985374166
Mehdi Benadel <mehdi.benadel@gmail.com>
2025-03-26 00:46:30 +0100

    forcing 8.2 support

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   name = "pixelfed-nix-shell";
-  buildInputs = with pkgs; [ act docker php83 php83Packages.composer nodejs nodePackages.npm mkcert ddev ];
+  buildInputs = with pkgs; [ act docker php82 php82Packages.composer nodejs nodePackages.npm mkcert ddev ];
   runScript = "$SHELL";
   shellHook = ''
       mkcert -install
