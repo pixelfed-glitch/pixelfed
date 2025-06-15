8c85dd0ef5b3e6863b4a5a111df9fd83a3fb338a
Nathan Higley <contact@nathanhigley.com>
2025-06-15 01:50:07 -0500

    fix: add home

--- app/Http/Middleware/RestrictedAccess.php
+++ app/Http/Middleware/RestrictedAccess.php
@@ -24,6 +24,7 @@ class RestrictedAccess
             if (!Auth::guard($guard)->check()) {
                 $p = [
                     'login',
+                    'home',
                     'password*',
                     'loginAs*',
                     'oauth/token',
