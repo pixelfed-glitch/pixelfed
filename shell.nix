ac7b86206b6f542c5851e7e0f89375f2498dc332
Nathan Higley <contact@nathanhigley.com>
2025-06-28 16:00:56 -0500

    fix: remove annoying cert gen

--- shell.nix
+++ shell.nix
@@ -4,7 +4,6 @@ pkgs.mkShell {
   buildInputs = with pkgs; [ act docker php84 php84Packages.composer nodejs nodePackages.npm mkcert ddev hadolint gh ];
   runScript = "$SHELL";
   shellHook = ''
-      mkcert -install
       export PATH="$PWD/node_modules/.bin/:$PATH"
       export PATH="$PWD/vendor/bin/:$PATH"
   '';
