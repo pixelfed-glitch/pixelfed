bf665d9d2219fb3136c483862a12b3cc2ca0e684
Shlee <github@shl.ee>
2025-12-06 21:52:36 +1030

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -78,8 +78,8 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => true,
             'options' => [
-                'request_checksum_calculation' => env('AWS_REQUEST_CHECKSUM_CALCULATION', 'ENABLED'),
-                'response_checksum_validation' => 'env('AWS_RESPONSE_CHECKSUM_VALIDATION', 'ENABLED'),
+                'request_checksum_calculation' => env('AWS_REQUEST_CHECKSUM_CALCULATION', 'WHEN_SUPPORTED'),
+                'response_checksum_validation' => env('AWS_RESPONSE_CHECKSUM_VALIDATION', 'WHEN_SUPPORTED'),
             ],
         ],
 
