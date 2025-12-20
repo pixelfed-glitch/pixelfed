030be10689a52925a488cd309c495fc3801296aa
Daniel Supernault <danielsupernault@gmail.com>
2025-12-20 01:00:54 -0700

    Fix RSP

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -48,10 +48,6 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
             ->namespace($this->namespace)
-            ->group(base_path('routes/dev.php'));
-
-        Route::middleware('web')
-            ->namespace($this->namespace)
             ->group(base_path('routes/web-admin.php'));
 
         Route::middleware('web')
