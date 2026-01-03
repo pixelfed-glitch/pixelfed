430e892b8cea69f69228f12ac643239f80475924
Mehdi Benadel <mehdi.benadel@gmail.com>
2026-01-04 00:59:36 +0100

    composer update  + flake update

--- shell.nix
+++ shell.nix
@@ -1,7 +1,39 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   name = "pixelfed-nix-shell";
-  buildInputs = with pkgs; [ act docker php84 php84Packages.composer nodejs nodePackages.npm mkcert ddev hadolint dgoss gh ];
+  buildInputs = with pkgs; [
+    act
+    docker
+    php84
+    php84Packages.composer
+    (pkgs.php84.buildEnv {
+      extensions = ({ enabled, all }: enabled ++ (with all; [
+        # bcmath
+        # curl
+        # gd
+        # imagick
+        # intl
+        # mbstring
+        # redis
+        # vips
+        # xml
+        # yaml
+        # zip
+        ffi
+        xdebug
+      ]));
+      extraConfig = ''
+        xdebug.mode=debug
+      '';
+    })
+    nodejs
+    nodePackages.npm
+    mkcert
+    ddev
+    hadolint
+    dgoss
+    gh
+  ];
   runScript = "$SHELL";
   shellHook = ''
       mkcert -install
